ab8214ea0449612a218fee9c67a0e268f5a2f85a
Patrik Ackland <patrikackland@gmail.com>
2015-07-14 21:27:30 +0200

    Added own key, css, more js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 function get_pictures() {
   $.ajax({
-    url: "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=efc9e30a247dfdc1367cba1ba68b3dae&extras=url_q&format=json&nojsoncallback=1",
+    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent' +
+    '&api_key=' + config.api_key +
+    '&extras=url_q' +
+    '&format=json' +
+    '&nojsoncallback=1',
     dataType: "text"
   })
   .done(function(data) {
     var json = JSON.parse(data);
     var div = document.getElementById('for_test')
     for (var i=0; i<json.photos.photo.length; i++) {
-      var url = 'https://farm' +
-        json.photos.photo[i].farm + '.staticflickr.com/' +
-        json.photos.photo[i].server + '/' +
-        json.photos.photo[i].id + '_' +
-        json.photos.photo[i].secret + '_q.jpg';
-      div.innerHTML = div.innerHTML + '<img src=' + url + '/>';
+      handle_photo(div, json.photos.photo[i], i);
     }
   });
+}
+
+function handle_photo(div, photo, i) {
+  var url = 'https://farm' +
+    photo.farm + '.staticflickr.com/' +
+    photo.server + '/' +
+    photo.id + '_' +
+    photo.secret + '_q.jpg';
+  var img = '<img width=100 height=100 id="img' + i + '" src=' + url + '/>';
+  var img_div = '<div id="img_div">' + img + '</div>';
+  div.innerHTML = div.innerHTML + img_div;
+  dominant_border(i);
+}
+
+function dominant_border(i) {
+  var img = document.getElementById('img' + i);
+  var pixels = getContext().getImageData(img.x,img.y,img.width,img.height).data;
+  img.style.borderColor = '#' + dec_to_hex(pixels[0]) + dec_to_hex(pixels[1]) + dec_to_hex(pixels[2]);
 }
